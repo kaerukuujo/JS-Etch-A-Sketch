53bb5692c38a9a09c75386069054ae0e58744eba
kaerukuujo <mrjolyne@gmail.com>
2022-11-23 15:52:26 +0000

    completed project, custom canvas size, restartable

--- script.js
+++ script.js
@@ -1,15 +1,37 @@
-const container = document.querySelector('#container');
-const rowBox = document.createElement('div');
-rowBox.classList.add('rowBox');
+//global var for amount of boxes
+let rows = 10;
+
+//pagecount is main container 
+const pageCont = document.querySelector('#pageContainer');
+
+const button = document.createElement('button');
+button.setAttribute('id', 'topBut');
+button.textContent = "change grid size";
+button.addEventListener("click", quest => {
+    rows = prompt("How many rows/columns ?");   
+    removeBoxes(); 
+    createBoxs(rows);
+});
+pageCont.appendChild(button);
 
 
+// function to create takes an argument of amount of squares user
+// wants to square to make grid
 
+function createBoxs (num) {  
+    
+    //container that cant include button because it gets completely removed on restart
+    const container = document.createElement('div');
+    container.setAttribute('id', 'container');
+    pageCont.appendChild(container);
 
-function createBoxs (num) {
+    const rowBox = document.createElement('div');
+    rowBox.classList.add('rowBox');
+
     for(i = 0; i < num; i++) {
         const rowDiv = document.createElement('div');
         rowDiv.setAttribute('onmouseenter', 'mouseOver(this)');
-        rowDiv.setAttribute('id', `${i}`);
+        rowDiv.setAttribute('id', 'drawBox');
         rowDiv.classList.add('rowDiv');
         rowBox.appendChild(rowDiv);
     }
@@ -26,87 +48,18 @@ function createBoxs (num) {
     }
 }
 
-createBoxs(16);
+//function for clearing canvas when new size has been entered
 
+function removeBoxes(){
+    if(document.getElementById('container') != null){
+        document.getElementById('container').remove();
+    }
+}
+
+// gets reference to current square, changes its class to 'hovered' to be altered in css.
 function mouseOver(ref){
     ref.classList.add('hovered');
 }
-
-container.appendChild(rowBox);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const container = document.querySelector('#container');
-// function createCol(num) {
-//     for(i = 0; i < num; i++){
-//         const colDiv = document.createElement('div');
-//         for(j = 0; j < num; j++){
-//             const rowDiv = document.createElement('div');
-//             rowDiv.classList.add('rowBox');
-//             colDiv.appendChild(rowDiv);
-//             //create row boxes
-//         }
-//         colDiv.classList.add('colBox');
-//         container.appendChild(colDiv);
-//     }
-// }
-
-// createCol(16);
-
-// const colBox = document.querySelector('#colBox');
-
-// const rowBox = document.querySelector('#rowBox');
-
-
-
-
-
-
-
-
-
-// const container = document.querySelector('#container');
-
-// function createDiv(num) {
-//     for(i = 0; i < num; i++){
-//         const colDiv = document.createElement('div');
-//         colDiv.classList.add('colBox');  
-//         container.appendChild(colDiv);
-//     }
-//     colDiv.forEach((colBox) => {
-//         for(j = 0; j < num; j++){
-//             const rowDiv = document.createElement('div');
-//             rowDiv.classList.add('rowBox');
-//             colBox.appendChild(rowDiv);
-//         }
-//     });    
-// }
-
-// createDiv(16);
-
-
-// const rowDiv = document.querySelector('#rowDiv');
-// rowDiv.classList.add('row');
-
-// for(i = 0; i < num; i++){
-//     const rowDiv = document.createElement('div');
-//     rowDiv.classList.add('rowBox');
-//     colDiv.appendChild(rowDiv);
-// }
 
 
 
